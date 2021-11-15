3c4cef05f98adbf9e1700c46528cd90727cec2af
ThunderSword99 <guardian.of.gods99@gmail.com>
2021-11-15 22:28:09 +0700

    feat(logic): add ragdoll for boss

--- Assets/_SuperheroRunner/Scripts/_GamePlay/Boss/BossController.cs
+++ Assets/_SuperheroRunner/Scripts/_GamePlay/Boss/BossController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -14,7 +15,33 @@ public class BossController : MonoBehaviour
     public BossAnim BossAnim;
     public Rigidbody Rigid;
     public TextMeshProUGUI LevelText;
+    public Collider MainCollider;
+    public List<Collider> ListCol;
+    public List<Rigidbody> ListRid;
 
+    public void Start()
+    {
+        ListCol.AddRange(transform.GetChild(0).GetChild(0).GetComponentsInChildren<Collider>());
+        ListRid.AddRange(transform.GetChild(0).GetChild(0).GetComponentsInChildren<Rigidbody>());
+        Setup();
+    }
+
+    public void Setup()
+    {
+        for (int i = 0; i < ListCol.Count-1; i++)
+        {
+            for (int j = i + 1; j < ListCol.Count; j++)
+            {
+                Physics.IgnoreCollision(ListCol[i],ListCol[j]);
+            }
+        }
+        for (int i = 0; i < ListCol.Count; i++)
+        {
+            ListRid[i].isKinematic = true;
+        }
+    }
+    
+    
     public void DoHitedAway(float force)
     {
         Rigid.velocity = Vector3.forward*force;
@@ -23,17 +50,29 @@ public class BossController : MonoBehaviour
 
     public void DoRagDoll()
     {
-        Rigid.constraints = RigidbodyConstraints.None;
-        Rigid.velocity = Vector3.forward;
+        Rigid.velocity = Vector3.zero;
+        BossAnim.Animacer.Animator.enabled = false;
+        ListCol.ForEach(item=>
+        {
+            item.enabled = false;
+            item.enabled = true;
+            //item.attachedRigidbody.velocity = Vector3.zero;
+            //item.attachedRigidbody.angularVelocity = Vector3.zero;
+        });
+        ListRid.ForEach(item=>
+        {
+            item.isKinematic = false;
+            //item.AddForce(Vector3.down);
+        });
     }
 
     private void FixedUpdate()
     {
         RaycastHit hit;
         
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, .5f, LayerMask.GetMask("Wall")))
+        if (Physics.Raycast(Rigid.transform.position, Rigid.transform.TransformDirection(Vector3.back), out hit, .5f, LayerMask.GetMask("Wall")))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.back) * hit.distance, Color.red);
+            Debug.DrawRay(Rigid.transform.position, Rigid.transform.TransformDirection(Vector3.back) * hit.distance, Color.red);
             BonusPlane bonusPlane = hit.transform.gameObject.GetComponent<BonusPlane>();
             bonusPlane.TurnOnPhysicWall();
             if (bonusPlane.LevelToReach+10 > GameManager.Instance.LevelController.CurrentLevel.Player.Level)
